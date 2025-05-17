121a297ef6a62e00d1e8c127bbb8d040a426a09c
于晨 <yuchen7701@foxmail.com>
2025-05-17 18:10:16 +0800

    feat: 调整页面刷新逻辑

--- BlazorApp1/BlazorApp1.Client/Data/MTemplateUnit.cs
+++ BlazorApp1/BlazorApp1.Client/Data/MTemplateUnit.cs
@@ -2,9 +2,49 @@
 
 public class MTemplateUnit
 {
-    public double Top { get; set; }
-    public double Left { get; set; }
-    public string Text { get; set; }
+    public Action<string, object>? FiledHasChanged;
+    private double _top;
+    private double _left;
+    private string _value = "QRCode";
+    public double Top
+    {
+        get { return _top; }
+        set
+        {
+            var hasChanged = _top != value;
+            if (hasChanged)
+            {
+                _top = value;
+                FiledHasChanged?.Invoke(nameof(Top), value);
+            }
+        }
+    }
+    public double Left
+    {
+        get { return _left; }
+        set
+        {
+            var hasChanged = _left != value;
+            if (hasChanged)
+            {
+                _left = value;
+                FiledHasChanged?.Invoke(nameof(Left), value);
+            }
+        }
+    }
+    public string Text
+    {
+        get { return _value; }
+        set
+        {
+            var hasChanged = _value != value;
+            if (hasChanged)
+            {
+                _value = value;
+                FiledHasChanged?.Invoke(nameof(Text), value);
+            }
+        }
+    }
     public bool IsSelected { get; set; }
     public UnitType UnitType { get; set; }
     public object? Value { get; set; }
